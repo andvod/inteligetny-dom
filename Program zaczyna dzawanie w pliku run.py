41ff34834ef17917ed9b63c5b810fc55d025e516
Andrii Vodvud <andvod@mail.ru>
2017-11-11 10:45:23 +0100

    second commit

--- Program zaczyna dzawanie w pliku run.py
+++
@@ -1,5 +0,0 @@
-Program zaczyna dzawanie w pliku run.py
-
-Wszystkie pliki powinny być umieszczone w jednym folderze na zdalnym komputerze, w danym przypadku Raspberry Pi2B.
-
-Program korzysta z bazy danych mySQL jak dodatkowe narzędzie.
